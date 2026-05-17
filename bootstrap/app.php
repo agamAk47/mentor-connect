8e5789676655bc83b3dcfdba3ec7ba9121d5f280
agamAk47 <agamkr.rakesh@gmail.com>
2026-05-18 04:00:29 +0530

    Ensure Vercel /tmp storage directories exist on cold start

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -24,6 +24,18 @@ $app = Application::configure(basePath: dirname(__DIR__))
 
 if (isset($_ENV['VERCEL']) || isset($_SERVER['VERCEL'])) {
     $app->useStoragePath('/tmp/storage');
+    
+    $directories = [
+        '/tmp/storage/framework/views',
+        '/tmp/storage/framework/cache/data',
+        '/tmp/storage/framework/sessions',
+        '/tmp/storage/logs',
+    ];
+    foreach ($directories as $dir) {
+        if (!is_dir($dir)) {
+            mkdir($dir, 0777, true);
+        }
+    }
 }
 
 return $app;
